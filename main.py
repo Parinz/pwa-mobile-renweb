54efd18147616e047f5c147f34d70e9cf24f1f06
Parinz <parinzee@protonmail.com>
2020-09-14 08:06:19 +0700

    Removed Unfinished parts of main.py

--- main.py
+++ main.py
@@ -5,19 +5,8 @@ main = Blueprint("main", __name__, static_folder="static/main/", template_folder
 
 @main.route("/", methods=["POST", "GET"])
 def login():
-    if request.method == "POST":
-        session.permanent = True
-        user = request.form["nm"]
-        session["user"] = user
-        return redirect(url_for("home"))
-    else:
-        if "user" in session:
-            return redirect(url_for("home"))
-
-        return render_template("signin.html")
     return render_template("signin.html")
 
-
-@main.route("/home", methods=["POST", "GET"])
+@main.route("/home/", methods=["POST", "GET"])
 def home():
     pass
