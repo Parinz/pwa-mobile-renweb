c3ad7b18285e18074f45c0fb3877519c607bd1af
parinz <parinzee@protonmail.com>
2020-10-09 13:03:11 +0700

    Added Basic Login Functionality

--- authenticate.py
+++ authenticate.py
@@ -1,80 +1,70 @@
+from flask.templating import render_template_string
 import requests
 from bs4 import BeautifulSoup
+from flask import url_for
 
-def Login():
-        District_Code = self.district_code.text
-        Username = self.username.text
-        Password = self.password.text
+def Login(District_Code, Username, Password):
+    try:
+        with requests.Session() as c:
+            District_Code = District_Code.upper()
+            Client_Code = District_Code.lower()
+            UserType = "PARENTSWEB-PARENT"
+            Submit = "Login"
+            formMethod = "login"
+            url = f"https://{Client_Code}.client.renweb.com/pwr/"
+            c.get(url)
+            login_data = {
+                "DistrictCode": District_Code,
+                "UserName": Username,
+                "Password": Password,
+                "UserType": UserType,
+                "Submit": Submit,
+                "formMethod": formMethod,
+            }
+            c.post(url, data=login_data)
+            urlpath = c.get(
+                f"https://{Client_Code}.client.renweb.com/pwr/student/index.cfm"
+            )
 
-        try:
-            with requests.Session() as c:
-                District_Code = District_Code.upper()
-                Client_Code = District_Code.lower()
-                UserType = "PARENTSWEB-PARENT"
-                Submit = "Login"
-                formMethod = "login"
-                url = f"https://{Client_Code}.client.renweb.com/pwr/"
-                c.get(url)
-                self.progress.value = 40
-                login_data = {
-                    "DistrictCode": District_Code,
-                    "UserName": Username,
-                    "Password": Password,
-                    "UserType": UserType,
-                    "Submit": Submit,
-                    "formMethod": formMethod,
-                }
-                self.progress.value = 80
-                c.post(url, data=login_data)
-                urlpath = c.get(
-                    f"https://{Client_Code}.client.renweb.com/pwr/student/index.cfm"
-                )
-                if (
-                    urlpath.url
-                    == f"https://{Client_Code}.client.renweb.com/pwr/student/index.cfm"
-                ):
-                    self.progress.value = 100
-                    M_App.screen_manager.current = "HomeScreen"
 
-                else:
-                    self.progress.value = 0
-                    self.dialog.open()
+            if (urlpath.url == f"https://{Client_Code}.client.renweb.com/pwr/student/index.cfm"):
+                return [District_Code, Username, Password]
 
-        except:
-            self.dialog.open()
+            else:
+                return -1
+    except:
+        return -1
 
-        if District_Code != "" and Username != "" and Password != "":
-            try:
-                with requests.Session() as c:
-                    District_Code = District_Code.upper()
-                    Client_Code = District_Code.lower()
-                    UserType = "PARENTSWEB-PARENT"
-                    Submit = "Login"
-                    formMethod = "login"
-                    url = f"https://{Client_Code}.client.renweb.com/pwr/"
+def GetData():
+  if District_Code != "" and Username != "" and Password != "":
+        with requests.Session() as c:
+            District_Code = District_Code.upper()
+            Client_Code = District_Code.lower()
+            UserType = "PARENTSWEB-PARENT"
+            Submit = "Login"
+            formMethod = "login"
+            url = f"https://{Client_Code}.client.renweb.com/pwr/"
 
-                    c.get(url)
-                    login_data = {
-                        "DistrictCode": District_Code,
-                        "UserName": Username,
-                        "Password": Password,
-                        "UserType": UserType,
-                        "Submit": Submit,
-                        "formMethod": formMethod,
-                    }
-                    c.post(url, data=login_data)
-                    page = c.get(
-                        f"https://{Client_Code}.client.renweb.com/pwr/student/index.cfm"
-                    ).text
-                    page = BeautifulSoup(page)
-                    page = page.find("table")
+            c.get(url)
+            login_data = {
+                "DistrictCode": District_Code,
+                "UserName": Username,
+                "Password": Password,
+                "UserType": UserType,
+                "Submit": Submit,
+                "formMethod": formMethod,
+            }
+            c.post(url, data=login_data)
+            page = c.get(
+                f"https://{Client_Code}.client.renweb.com/pwr/student/index.cfm"
+            ).text
+            page = BeautifulSoup(page)
+            page = page.find("table")
 
-                    tablerows = page.find_all("tr")
-                    for tr in tablerows:
-                        td = tr.find_all("td")
-                        row = [i.text for i in td]
-                        row = tuple(list(map(lambda s: s.strip("\n"), row)))
-                        g_list.append(row)
-                        print(row)
-            except:
-                self.dialog.open()
+            tablerows = page.find_all("tr")
+            for tr in tablerows:
+                td = tr.find_all("td")
+                row = [i.text for i in td]
+                row = tuple(list(map(lambda s: s.strip("\n"), row)))
+                g_list.append(row)
+                print(row)
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from flask import Blueprint, render_template, request, session, url_for, redirect
 from datetime import timedelta
-
 from flask.templating import render_template_string
+
+import authenticate
 
 main = Blueprint("main", __name__, static_folder="static/main/", template_folder="templates/main/")
 
 @main.route("/", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        return render_template_string("<h1> IT WORKS </h1>")
-    return render_template("signin.html")
+        Values = authenticate.Login(request.form['DistrictCode'], request.form['Username'], request.form['Password'])
+        if Values == -1:
+            return render_template_string("Wrong Pass")
+        else:
+            return render_template_string("Functioning Properly")
+    else:
+        return render_template("signin.html")
+
+@main.route("/cookies", methods=["GET"])
+def cookies():
+    return render_template_string("<h1> IT WORKS </h1>")
 
 @main.route("/home/", methods=["POST", "GET"])
 def home():
