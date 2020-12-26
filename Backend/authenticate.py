3852659dceb8ba856cdb3158eb75df29dd237873
Parinz <parinzee@protonmail.com>
2020-12-26 11:32:50 +0700

    Something's not right, my code ran on the first try lmao

--- Backend/authenticate.py
+++ Backend/authenticate.py
@@ -34,8 +34,28 @@ def Login(District_Code, Username, Password):
     except:
         return -2
 
-def GetData(District_Code, Username, Password):
-  if District_Code != "" and Username != "" and Password != "":
+def GetData(Client_Code, Request_Object):
+            page = Request_Object.get(
+                f"https://{Client_Code}.client.renweb.com/pwr/student/index.cfm"
+            ).text
+            page = BeautifulSoup(page, 'lxml')
+            page = page.find_all("table")
+            foobar = []
+            for tables in page:
+                g_list = []
+                tableBody = tables.find_all("tbody")
+                for tr in tableBody:
+                    for foo in tr.find_all("tr"):
+                        td = foo.find_all("td")
+                        row = [i.text for i in td]
+                        row = list(map(lambda s: s.strip("\n"), row))
+                        g_list.append(row)
+                foobar.append(g_list)
+
+            return foobar
+
+def globalGetData(District_Code, Username, Password):
+    if District_Code != "" and Username != "" and Password != "":
         with requests.Session() as c:
             District_Code = District_Code.upper()
             Client_Code = District_Code.lower()
@@ -54,21 +74,6 @@ def GetData(District_Code, Username, Password):
                 "formMethod": formMethod,
             }
             c.post(url, data=login_data)
-            page = c.get(
-                f"https://{Client_Code}.client.renweb.com/pwr/student/index.cfm"
-            ).text
-            page = BeautifulSoup(page, 'lxml')
-            page = page.find_all("table")
-            foobar = []
-            for tables in page:
-                g_list = []
-                tableBody = tables.find_all("tbody")
-                for tr in tableBody:
-                    for foo in tr.find_all("tr"):
-                        td = foo.find_all("td")
-                        row = [i.text for i in td]
-                        row = list(map(lambda s: s.strip("\n"), row))
-                        g_list.append(row)
-                foobar.append(g_list)
 
-            return foobar
+            return GetData(Client_Code, c)
+
